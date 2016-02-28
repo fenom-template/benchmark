41c460b4c0c49a66f431c8343450ff66d29c0dc1
maxicms <support@maxicms.ru>
2016-02-28 11:48:19 +0200

    update benchmark
    update templates engine.
    use color php console class

--- bin/fenom-benchmark.php
+++ bin/fenom-benchmark.php
@@ -1,8 +1,9 @@
 <?php
 
+use Colors\Color;
 use Fenom\Benchmark;
 
-require(__DIR__.'/../vendor/autoload.php');
+require __DIR__ . '/../vendor/autoload.php';
 
 $opt = getopt("h", array(
     /** @var string $message */
@@ -15,69 +16,72 @@ $opt = getopt("h", array(
     "markdown",
     /** @vat boolean $help */
     /** @vat boolean $h */
-    "help"
+    "help",
 ));
 
 $opt += array(
-    "stress" => 0
+    "stress" => 0,
 );
 
 extract($opt);
 
+$c = new Color();
 
+$c->setTheme(
+    array(
+        'header' => array('red'),
+        'title' => array('magenta'),
+        'done' => 'green',
+    )
+);
 
-if(isset($h) || isset($help)) {
+if (isset($h) || isset($help)) {
     echo "
-Start: ".basename(__FILE__)." [--stress COUNT] [--auto_reload] [--cleanup] [--markdown]
-Usage: ".basename(__FILE__)." [--help | -h]
+Start: " . basename(__FILE__) . " [--stress COUNT] [--auto_reload] [--cleanup] [--markdown]
+Usage: " . basename(__FILE__) . " [--help | -h]
 ";
     exit;
 }
+
+echo "\033c";
 
 Benchmark::init();
 
 Benchmark::$stress = intval($stress);
 Benchmark::$auto_reload = isset($auto_reload);
 
-exec("rm -rf ".__DIR__."/../compile/*");
+exec("rm -rf " . __DIR__ . "/../compile/*");
 
-echo "Smarty3 vs Twig vs Fenom\n\n";
+echo $c('Smarty3 vs Twig vs Fenom')->header->bold . PHP_EOL . PHP_EOL;
 
-echo "Generate templates... ";
-passthru("php ".Benchmark::$tpl_path."/inheritance/smarty.gen.php");
-passthru("php ".Benchmark::$tpl_path."/inheritance/twig.gen.php");
-echo "Done\n";
+echo $c('Generate templates...')->title->bold . PHP_EOL;
+passthru("php " . Benchmark::$tpl_path . "/inheritance/smarty.gen.php");
+passthru("php " . Benchmark::$tpl_path . "/inheritance/twig.gen.php");
+echo $c('Done')->done . PHP_EOL . PHP_EOL;
 
-echo "Testing a lot output...\n";
+echo $c('Testing a lot output...')->title->bold . PHP_EOL;
 
-Benchmark::runs("smarty3", 'echo/smarty.tpl',   Benchmark::$tpl_path.'/echo/data.json');
-Benchmark::runs("twig",    'echo/twig.tpl',     Benchmark::$tpl_path.'/echo/data.json');
-Benchmark::runs("fenom",   'echo/smarty.tpl',   Benchmark::$tpl_path.'/echo/data.json');
-//if(extension_loaded("phalcon")) {
-//    Benchmark::runs("volt",   'echo/twig.tpl',  __DIR__.'/templates/echo/data.json');
-//}
-echo "\nTesting 'foreach' of big array...\n";
+Benchmark::runs("smarty3", 'echo/smarty.tpl', Benchmark::$tpl_path . '/echo/data.json');
+Benchmark::runs("twig", 'echo/twig.tpl', Benchmark::$tpl_path . '/echo/data.json');
+Benchmark::runs("fenom", 'echo/smarty.tpl', Benchmark::$tpl_path . '/echo/data.json');
 
-Benchmark::runs("smarty3", 'foreach/smarty.tpl', Benchmark::$tpl_path.'/foreach/data.json');
-Benchmark::runs("twig",    'foreach/twig.tpl',   Benchmark::$tpl_path.'/foreach/data.json');
-Benchmark::runs("fenom",   'foreach/smarty.tpl', Benchmark::$tpl_path.'/foreach/data.json');
-//if(extension_loaded("phalcon")) {
-//    Benchmark::runs("volt",   'foreach/twig.tpl', __DIR__.'/templates/foreach/data.json');
-//}
+echo $c('Testing "foreach" of big array...')->title->bold . PHP_EOL;
 
-echo "\nTesting deep 'inheritance'...\n";
+Benchmark::runs("smarty3", 'foreach/smarty.tpl', Benchmark::$tpl_path . '/foreach/data.json');
+Benchmark::runs("twig", 'foreach/twig.tpl', Benchmark::$tpl_path . '/foreach/data.json');
+Benchmark::runs("fenom", 'foreach/smarty.tpl', Benchmark::$tpl_path . '/foreach/data.json');
 
-Benchmark::runs("smarty3", 'inheritance/smarty/b100.tpl', Benchmark::$tpl_path.'/foreach/data.json');
-Benchmark::runs("twig",    'inheritance/twig/b100.tpl', Benchmark::$tpl_path.'/foreach/data.json');
-Benchmark::runs("fenom",  'inheritance/smarty/b100.tpl', Benchmark::$tpl_path.'/foreach/data.json');
-//if(extension_loaded("phalcon")) {
-//    Benchmark::runs("volt",  'inheritance/twig/b100.tpl', __DIR__.'/templates/foreach/data.json');
-//}
+echo $c("Testing deep 'inheritance'...")->title->bold . PHP_EOL;
 
-echo "\nDone\n";
-if(isset($cleanup)) {
+Benchmark::runs("smarty3", 'inheritance/smarty/b100.tpl', Benchmark::$tpl_path . '/foreach/data.json');
+Benchmark::runs("twig", 'inheritance/twig/b100.tpl', Benchmark::$tpl_path . '/foreach/data.json');
+Benchmark::runs("fenom", 'inheritance/smarty/b100.tpl', Benchmark::$tpl_path . '/foreach/data.json');
+
+echo $c('Done')->done . PHP_EOL . PHP_EOL;
+
+if (isset($cleanup)) {
     echo "Cleanup.\n";
-    passthru("rm -rf ".__DIR__."/../compile/*");
-    passthru("rm -f ".__DIR__."/../templates/inheritance/smarty/*");
-    passthru("rm -f ".__DIR__."/../templates/inheritance/twig/*");
+    passthru("rm -rf " . __DIR__ . "/../compile/*");
+    passthru("rm -f " . __DIR__ . "/../templates/inheritance/smarty/*");
+    passthru("rm -f " . __DIR__ . "/../templates/inheritance/twig/*");
 }
--- src/Benchmark.php
+++ src/Benchmark.php
@@ -2,7 +2,8 @@
 
 namespace Fenom;
 
-class Benchmark {
+class Benchmark
+{
     const OUTPUT = "%8s: %-22s %10.4f sec, %10.1f MiB\n";
 
     public static $stress = 0;
@@ -11,27 +12,29 @@ class Benchmark {
     public static $tpl_path;
     public static $bin_path;
 
-    public static function init() {
-        self::$tpl_path = dirname(__DIR__).'/templates';
-        self::$cmp_path = dirname(__DIR__).'/compile';
-        self::$bin_path = dirname(__DIR__).'/bin';
+    public static function init()
+    {
+        self::$tpl_path = dirname(__DIR__) . '/templates';
+        self::$cmp_path = dirname(__DIR__) . '/compile';
+        self::$bin_path = dirname(__DIR__) . '/bin';
     }
 
-    public static function smarty3($tpl, $data, $double, $stress = false, $auto_reload = false) {
+    public static function smarty3($tpl, $data, $double, $stress = false, $auto_reload = false)
+    {
         $smarty = new \Smarty();
         $smarty->compile_check = $auto_reload;
 
         $smarty->setTemplateDir(self::$tpl_path);
         $smarty->setCompileDir(self::$cmp_path);
 
-        if($double) {
+        if ($double) {
             $smarty->assign($data);
             $smarty->fetch($tpl);
         }
 
         $start = microtime(true);
-        if($stress) {
-            for($i=0; $i<$stress; $i++) {
+        if ($stress) {
+            for ($i = 0; $i < $stress; $i++) {
                 $smarty->assign($data);
                 $smarty->fetch($tpl);
             }
@@ -44,9 +47,9 @@ class Benchmark {
 //        printf(self::$t, __FUNCTION__, $message, round(microtime(true)-$start, 4), round(memory_get_peak_usage()/1024/1024, 2));
     }
 
-    public static function twig($tpl, $data, $double, $stress = false, $auto_reload = false) {
+    public static function twig($tpl, $data, $double, $stress = false, $auto_reload = false)
+    {
 
-        \Twig_Autoloader::register();
         $loader = new \Twig_Loader_Filesystem(self::$tpl_path);
         $twig = new \Twig_Environment($loader, array(
             'cache' => self::$cmp_path,
@@ -54,13 +57,13 @@ class Benchmark {
             'auto_reload' => $auto_reload,
         ));
 
-        if($double) {
+        if ($double) {
             $twig->loadTemplate($tpl)->render($data);
         }
 
         $start = microtime(true);
-        if($stress) {
-            for($i=0; $i<$stress; $i++) {
+        if ($stress) {
+            for ($i = 0; $i < $stress; $i++) {
                 $twig->loadTemplate($tpl)->render($data);
             }
         } else {
@@ -69,18 +72,19 @@ class Benchmark {
         return microtime(true) - $start;
     }
 
-    public static function fenom($tpl, $data, $double, $stress = false, $auto_reload = false) {
+    public static function fenom($tpl, $data, $double, $stress = false, $auto_reload = false)
+    {
 
         $fenom = \Fenom::factory(self::$tpl_path, self::$cmp_path);
-        if($auto_reload) {
+        if ($auto_reload) {
             $fenom->setOptions(\Fenom::AUTO_RELOAD);
         }
-        if($double) {
+        if ($double) {
             $fenom->fetch($tpl, $data);
         }
         $start = microtime(true);
-        if($stress) {
-            for($i=0; $i<$stress; $i++) {
+        if ($stress) {
+            for ($i = 0; $i < $stress; $i++) {
                 $fenom->fetch($tpl, $data);
             }
         } else {
@@ -90,29 +94,30 @@ class Benchmark {
     }
 
 //    public static function volt($tpl, $data, $double, $message) {
-//        $view = new \Phalcon\Mvc\View();
-//        //$view->setViewsDir(__DIR__.'/../templates');
-//        $volt = new \Phalcon\Mvc\View\Engine\Volt($view);
-//
-//
-//        $volt->setOptions(array(
-//            "compiledPath" => __DIR__.'/../compile'
-//        ));
-//        $tpl = __DIR__.'/../templates/'.$tpl;
-//        if($double) {
-//            $volt->render($tpl, $data);
-//        }
-//
-//        $start = microtime(true);
-//        $volt->render($tpl, $data);
-//        printf(self::$t, __FUNCTION__, $message, round(microtime(true)-$start, 4), round(memory_get_peak_usage()/1024/1024, 2));
-//    }
+    //        $view = new \Phalcon\Mvc\View();
+    //        //$view->setViewsDir(__DIR__.'/../templates');
+    //        $volt = new \Phalcon\Mvc\View\Engine\Volt($view);
+    //
+    //
+    //        $volt->setOptions(array(
+    //            "compiledPath" => __DIR__.'/../compile'
+    //        ));
+    //        $tpl = __DIR__.'/../templates/'.$tpl;
+    //        if($double) {
+    //            $volt->render($tpl, $data);
+    //        }
+    //
+    //        $start = microtime(true);
+    //        $volt->render($tpl, $data);
+    //        printf(self::$t, __FUNCTION__, $message, round(microtime(true)-$start, 4), round(memory_get_peak_usage()/1024/1024, 2));
+    //    }
 
-    public static function run($engine, $template, $data, $double, $message) {
+    public static function run($engine, $template, $data, $double, $message)
+    {
         if (getenv('TRAVIS')) {
             passthru(sprintf("$(phpenv which php) -n -ddate.timezone=Europe/Moscow -dmemory_limit=512M %s/engine.php --engine '%s' --template '%s' --data '%s' --message '%s' %s --stress %d %s", self::$bin_path, $engine, $template, $data, $message, $double ? '--double' : '', self::$stress, self::$auto_reload ? '--auto_reload' : ''));
         } else {
-            passthru(sprintf(PHP_BINARY." -n -ddate.timezone=Europe/Moscow -dmemory_limit=512M %s/engine.php --engine '%s' --template '%s' --data '%s' --message '%s' %s --stress %d %s", self::$bin_path, $engine, $template, $data, $message, $double ? '--double' : '', self::$stress, self::$auto_reload ? '--auto_reload' : ''));
+            passthru(sprintf(PHP_BINARY . " -n -ddate.timezone=Europe/Moscow -dmemory_limit=512M %s/engine.php --engine '%s' --template '%s' --data '%s' --message '%s' %s --stress %d %s", self::$bin_path, $engine, $template, $data, $message, $double ? '--double' : '', self::$stress, self::$auto_reload ? '--auto_reload' : ''));
         }
     }
 
@@ -121,10 +126,11 @@ class Benchmark {
      * @param $template
      * @param $data
      */
-    public static function runs($engine, $template, $data) {
+    public static function runs($engine, $template, $data)
+    {
         self::run($engine, $template, $data, false, '!compiled and !loaded');
         self::run($engine, $template, $data, false, ' compiled and !loaded');
-        self::run($engine, $template, $data, true,  ' compiled and  loaded');
+        self::run($engine, $template, $data, true, ' compiled and  loaded');
         echo "\n";
     }
 }
